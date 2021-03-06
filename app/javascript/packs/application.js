a3014a44e7de1080e998fe272e199745196eec0f
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-03-06 14:20:37 +0900

    取り消し３

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require ("jquery3").start()
+require('jquery')
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,3 +18,10 @@ require ("jquery3").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require turbolinks
+//= require jquery 
+//= require jquery_ujs 
+//= require_tree .
+
+
